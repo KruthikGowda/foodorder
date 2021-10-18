8f95d6cc4f5daa86326929d46bc0bac8c3dd7df2
Kruthik Gowda <prkruthikgowda@gmail.com>
2021-10-18 19:28:26 +0530

    Commit

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Coverimage from "../../assets/images/meals.jpg";
+import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
+
+export default function Header(props) {
+  return (
+    <React.Fragment>
+      <header className={classes.header}>
+        <h1>HungryMeals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={Coverimage} alt="CoverImage" />
+      </div>
+    </React.Fragment>
+  );
+}
