679f2ed26383482b7de5b896bb50d6730621e614
Kruthik Gowda <prkruthikgowda@gmail.com>
2021-10-25 10:28:55 +0530

    commit

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -2,23 +2,39 @@ import React, { useContext, useReducer } from "react";
 import CartContext from "./cart-context";
 
 const cartReducer = (state, action) => {
+  if (action.type === "ADD_CART_ITEM") {
+    const updatedItems = state.items.concat(action.item);
+    const updatedTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
   return defaultCartState;
 };
 
 const defaultCartState = {
-  item: [],
+  items: [],
   totalAmount: 0,
 };
 
 export default function CartProvider(props) {
-  useReducer(cartReducer, defaultCartState);
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
 
-  const addItems = (item) => {};
+  const addItems = (item) => {
+    dispatchCartAction({ type: "ADD_CART_ITEM", item: item });
+  };
 
-  const removeItems = (id) => {};
+  const removeItems = (id) => {
+    dispatchCartAction({ type: "REMOVE_CART_ITEM", id: id });
+  };
 
   const cartContext = {
-    items: [],
+    items: cartState.items,
     totalAmount: 0,
     addItem: addItems,
     removeItem: removeItems,
